a2125a7604dafb0565397c29071b1c428d340659
Denis Angell <dangell@transia.co>
2023-07-01 13:04:12 -0400

    update hooks

--- contracts/toolbox/keylet_owner_dir.c
+++ contracts/toolbox/keylet_owner_dir.c
@@ -8,62 +8,45 @@ int64_t hook(uint32_t reserved) {
     uint8_t hook_accid[20];
     hook_account(SBUF(hook_accid));
 
-    int64_t count[1];
-    count[0]++;
+    uint8_t owner_dir[34];
+    util_keylet(SBUF(owner_dir), KEYLET_OWNER_DIR, hook_accid, SFS_ACCOUNT, 0, 0, 0, 0);
 
-    state_set(SBUF(count), hook_accid, SFS_ACCOUNT);
+    TRACEHEX(owner_dir);
 
-    // uint8_t issuer_accid[20];
-    // int64_t ret = util_accid(SBUF(issuer_accid), SBUF("rK7FWrmja5GYU1CY3Z3cTv92v7E74K6Nqy"));
-    // TRACEVAR(ret);
-    // TRACEHEX(issuer_accid);
-
-    // KEYLET: TrustLine
-    uint8_t namespace[32] = {
-        0xDAU,0x4AU,0x3CU,0x64U,0x92U,0x0FU,0xC1U,0xEDU,0xCDU,0xD7U,
-        0x2DU,0x9CU,0x5CU,0x8EU,0x9CU,0x78U,0x0EU,0x07U,0x6FU,0xF4U,
-        0x4BU,0x2CU,0x81U,0x02U,0xE8U,0xE0U,0xFDU,0x63U,0x66U,0x52U,
-        0x63U,0xD1U
-    };
-
-    uint8_t hook_state_dir[34];
-    util_keylet(SBUF(hook_state_dir), KEYLET_HOOK_STATE_DIR, hook_accid, SFS_ACCOUNT,  SBUF(namespace), 0, 0);
-
-    TRACEHEX(hook_state_dir);
-    // TRACEVAR(slot_set(SBUF(hook_state_dir), 1));
-    // uint8_t owner_dir_kl[34];
-    // util_keylet(SBUF(owner_dir_kl), KEYLET_OWNER_DIR, hook_accid, SFS_ACCOUNT,  0, 0, 0, 0);
-
-    // // SLOT SET:
-    if (slot_set(SBUF(hook_state_dir), 1) != 1)
+    // SLOT SET:
+    if (slot_set(SBUF(owner_dir), 1) != 1)
         accept(SBUF("keylet_owner_dir: Could not load keylet"), __LINE__);
 
     if (slot_subfield(1, sfIndexes, 2) != 2)
-            accept(SBUF("keylet_owner_dir: Could not load sfIndexes"), __LINE__);
+        accept(SBUF("keylet_owner_dir: Could not load sfIndexes"), __LINE__);
 
-    // TRACEVAR(slot_count(1));
-    // uint8_t entry_type[2];
-    // int64_t bytes_written = slot(SBUF(entry_type), 2);
-    // TRACEHEX(entry_type);
-    // TRACESTR(entry_type);
-    // TRACEVAR(slot_subarray(1, sfIndexes, 2));
-    // slot_subarray(1, sfIndexes, 2);
+    // uint8_t arrays[65];
+    // int64_t arrays_len = slot(SBUF(arrays), 2);
+    // TRACEVAR(arrays_len);
+    // TRACEHEX(arrays);
 
-    // if (slot_subarray(1, sfIndexes, 2) != 2)
-    // {
-    //     accept(SBUF("keylet_owner_dir: Could not load sfIndexes"), __LINE__);
-    // } 
-    // else
-    // {
-    //     TRACESTR("SLOT INDEXS");
-    //     // SLOT SUBFIELD: sfTakerGets
-    //     if (slot_subfield(2, sfTakerGets, 3) != 3)
-    //         accept(SBUF("keylet_owner_dir: Could not load target balance 2"), __LINE__);
+    // 40 2448F6F57C6B3636716BDE3AD860EC7C92D1B029006770E7F396234469324934F86293A1654AC5F6A5E18261308D7409E4302131F6CB0C97D78261E2E14827BC
+    // 2448F6F57C6B3636716BDE3AD860EC7C92D1B029006770E7F396234469324934
+    // F86293A1654AC5F6A5E18261308D7409E4302131F6CB0C97D78261E2E14827BC
 
-    //     int64_t balance = slot_float(3); // <- amount as xrp
-    //     TRACEVAR(balance);
-    // }
+    uint8_t index1[32] = {
+        0xF8, 0x62, 0x93, 0xA1, 0x65, 0x4A, 0xC5, 0xF6,
+        0xA5, 0xE1, 0x82, 0x61, 0x30, 0x8D, 0x74, 0x09,
+        0xE4, 0x30, 0x21, 0x31, 0xF6, 0xCB, 0x0C, 0x97,
+        0xD7, 0x82, 0x61, 0xE2, 0xE1, 0x48, 0x27, 0xBC
+    };
 
+    uint8_t dir_1[34];
+    util_keylet(SBUF(dir_1), KEYLET_UNCHECKED, SBUF(index1),  0, 0, 0, 0);
+
+    if (slot_set(SBUF(dir_1), 3) != 3)
+        accept(SBUF("keylet_owner_dir: Could not load inner keylet"), __LINE__);
+
+    if (slot_subfield(3, sfBalance, 4) != 4)
+        accept(SBUF("keylet_owner_dir: Could not load sfBalance"), __LINE__);
+    
+    int64_t balance = float_int(slot_float(4), 0, 1);
+    TRACEVAR(balance);
     // Your code here...
 
     TRACESTR("keylet_owner_dir: End.");
