81d1adec5f1dee8503b4d3168b4d1644aeefefeb
Denis Angell <dangell@transia.co>
2024-01-26 14:19:36 +0100

    Update TestModel.ts

--- src/libs/binary-models/TestModel.ts
+++ src/libs/binary-models/TestModel.ts
@@ -3,24 +3,28 @@ import { UInt64, VarString } from './utils/types'
 
 export class TestModel extends BaseModel {
   updatedTime: UInt64
+  updatedBy: VarString
   message: VarString
 
-  constructor(updatedTime: UInt64, message: VarString) {
+  constructor(updatedTime: UInt64, updatedBy: VarString, message: VarString) {
     super()
     this.updatedTime = updatedTime
+    this.updatedBy = updatedBy
     this.message = message
   }
 
   getMetadata(): Metadata {
     return [
       { field: 'updatedTime', type: 'uint64' },
-      { field: 'message', type: 'varString', maxStringLength: 20 },
+      { field: 'updatedBy', type: 'varString', maxStringLength: 32 },
+      { field: 'message', type: 'varString', maxStringLength: 250 },
     ]
   }
 
   toJSON() {
     return {
       updatedTime: this.updatedTime,
+      updatedBy: this.updatedBy,
       message: this.message,
     }
   }
