c7e65dd810883cf85b5072b98517a01c66b4021c
Denis Angell <dangell@transia.co>
2024-02-29 16:02:06 +0100

    add hookOff

--- src/utils.ts
+++ src/utils.ts
@@ -17,6 +17,10 @@ import {
   Amount,
   UInt32,
 } from '@transia/ripple-binary-codec/dist/types'
+import {
+  TRANSACTION_TYPES,
+  TRANSACTION_TYPE_MAP,
+} from '@transia/ripple-binary-codec'
 import { createHash } from 'crypto'
 
 export async function getXRPBalance(
@@ -289,4 +293,43 @@ export function generateHash(dataBytes: Buffer) {
 export function padHexString(input: string, targetLength = 64): string {
   const paddedString = '0'.repeat(targetLength - input.length) + input
   return paddedString
+}
+
+/**
+ * @constant tts
+ * @description
+ * Transaction types
+ */
+
+/**
+ * @typedef TTS
+ * @description
+ * Transaction types
+ */
+export type TTS = typeof TRANSACTION_TYPE_MAP
+
+export function calculateHookOff(arr: Array<keyof TTS>): string {
+  let hash =
+    '0x0000000000000000000000000000000000000000000000000000000000000000'
+  arr.forEach((nth) => {
+    if (typeof nth !== 'string') {
+      throw new Error(`HookOn transaction type must be string`)
+    }
+    if (!TRANSACTION_TYPES.includes(String(nth))) {
+      throw new Error(
+        `invalid transaction type '${String(nth)}' in HookOn array`
+      )
+    }
+
+    const tts: Record<string, number> = TRANSACTION_TYPE_MAP
+    let value = BigInt(hash)
+    // eslint-disable-next-line no-bitwise -- Required
+    value ^= BigInt(1) << BigInt(tts[nth])
+    // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Required
+    hash = `0x${value.toString(16)}`
+  })
+  hash = hash.replace('0x', '')
+  // eslint-disable-next-line @typescript-eslint/no-magic-numbers -- Required
+  hash = hash.padStart(64, '0')
+  return hash.toUpperCase()
 }
