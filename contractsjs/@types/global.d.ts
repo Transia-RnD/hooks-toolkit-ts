53120345d62baa028026210f29a2682ca27fa004
Denis Angell <dangell@transia.co>
2024-05-30 21:35:50 +0200

    move types

--- contractsjs/@types/global.d.ts
+++ contractsjs/@types/global.d.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { Transaction } from '@transia/xrpl-models'
+import { Transaction } from '@transia/xahau-models'
 
 declare global {
   /********************************************************************************************************************* */
--- contractsjs/toolbox/base.ts
+++ contractsjs/toolbox/base.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// @ts-expect-error -- ignore
 const Hook = (arg) => {
   trace('Base.ts: Called.', false)
   return accept('base.ts: Finished.', 18)
