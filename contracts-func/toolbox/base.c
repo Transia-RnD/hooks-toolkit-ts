5d43eb1f3231e42747a3037094c028da3a488d23
Denis Angell <dangell@transia.co>
2025-05-08 09:35:08 +0200

    Update base.c

--- contracts-func/toolbox/base.c
+++ contracts-func/toolbox/base.c
@@ -1,12 +1,12 @@
 #include "hookapi.h"
 
-int64_t hook_one(uint32_t reserved) {
+int64_t func_one(uint32_t reserved) {
     TRACESTR("func_one.c: Called.");
     _g(1,1);
     return accept(SBUF("func_one: Finished."), __LINE__);
 }
 
-int64_t hook_two(uint32_t reserved) {
+int64_t func_two(uint32_t reserved) {
     TRACESTR("func_two.c: Called.");
     _g(1,1);
     return accept(SBUF("func_two: Finished."), __LINE__);
